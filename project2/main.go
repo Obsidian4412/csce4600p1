7c8aa6febe5b1deaa809632d60d0be8ca88948c5
Obsidian4412 <103223749+Obsidian4412@users.noreply.github.com>
2023-12-10 10:31:23 -0600

    Update main.go

--- project2/main.go
+++ project2/main.go
@@ -73,6 +73,8 @@ func handleInput(w io.Writer, input string, exit chan<- struct{}) error {
 
 	// Check for built-in commands.
 	// New builtin commands should be added here. Eventually this should be refactored to its own func.
+	//using bash builtins
+	
 	switch name {
 	case "cd":
 		return builtins.ChangeDirectory(args...)
